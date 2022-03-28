1de2dadcd22f57ef66f709042d1565197de189d5
dhkim <donghkim831@gmail.com>
2022-03-28 18:35:07 +0900

    Added splash and app icon.

---
+++ app/src/main/java/cloud/shoplive/sample/SplashActivity.kt
@@ -0,0 +1,20 @@
+package cloud.shoplive.sample
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 500)
+    }
+}
